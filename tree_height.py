a167cc711a5fc73b143f699aeb1e0061b1628743
Kirils Aleksejevs <102963681+letseatmeat@users.noreply.github.com>
2023-03-06 00:33:20 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,16 +2,39 @@
 
 import sys
 import threading
-
-
+        
 def compute_height(n, parents):
     # Write this function
+    x = {}
+    def treeheight(i):
+        if i in x:
+            return x[i]
+        
+        if i == -1:
+            return 0
+        hx = 1 + treeheight(parents[i])
+        x[i] = hx
+        return
     max_height = 0
     # Your code here
+    for i in range(s):
+        max_height = max(max_height, height(i))
     return max_height
 
-
 def main():
+    y = input()
+    if "I" in y:
+        z = int(input())
+        inp = list(map(int,input().split()))
+        print(compute_height(z,inp))
+    if "F" in y:
+        f1 = input()
+        if "a" not in f1:
+            with open("./test/" + f, "r") as f2:
+                e = int(f2.readLine())
+                out=list(map(int, f2.readLine().split()))
+                print(compute_height(z,out))
+    
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
